2eceba3e13228b25c3e7d44688aca823ba3f4aae
rjatkinson2 <rjatkinson2@gmail.com>
2015-02-04 15:57:41 -0800

    Completed function definition for queue.js and stack.js for functional-sharing type.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,28 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.count = 0;
+  storage.offset = 0;
+  storage.enqueue = queueMethods.enqueue;
+  storage.dequeue = queueMethods.dequeue;
+  storage.size = queueMethods.size;
+  return storage;
 };
 
 var queueMethods = {};
-
-
+queueMethods.enqueue = function(item) {
+  this[(this.count+this.offset)] = item;
+  this.count++;
+};
+queueMethods.dequeue = function() {
+  this.count && this.count--;
+  var temp = this[this.offset];
+  delete this[this.offset];
+  this.offset++;
+  return temp;
+};
+queueMethods.size = function() {
+  return this.count;
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.count = 0;
+  storage.offset = 0;
+  storage.push = stackMethods.push;
+  storage.pop = stackMethods.pop;
+  storage.size = stackMethods.size;
+  return storage;
 };
 
 var stackMethods = {};
-
-
+stackMethods.push = function(item){
+  this[this.count] = item;
+  this.count++;
+};
+stackMethods.pop = function(){
+  this.count && this.count--;
+  return this[this.count];
+};
+stackMethods.size = function(){
+  return this.count;
+};
