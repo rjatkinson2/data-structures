120c28bf933706e23e7b182b1484a03805d730e7
rjatkinson2 <rjatkinson2@gmail.com>
2015-02-04 21:56:47 -0800

    Completed function definition for prototypal version of stack. Made significant progress on function definition for prototypal version of queue, but the size method is not working properly.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var object = Object.create(queueMethods);
+  object.count = 0;
+  object.offset = 0;
+  return object;
 };
 
 var queueMethods = {};
+queueMethods.queue = function(val){
+  this[this.count + this.offset] = val;
+  this.count++;
+};
 
+queueMethods.dequeue = function(){
+  this.count && this.count--;
+  var temp = this[this.offset];
+  delete this[this.offset];
+  this.offset++;
+  return temp;
+};
+
+queueMethods.size = function(){
+  return this.count;
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var object = Object.create(stackMethods);
+  object.count = 0;
+  return object;
 };
 
 var stackMethods = {};
-
+stackMethods.push = function(val) {
+  this[this.count] = val;
+  this.count++;
+};
+stackMethods.pop = function() {
+  this.count && this.count--;
+  return this[this.count];
+};
+stackMethods.size = function() {
+  return this.count;
+};
 
