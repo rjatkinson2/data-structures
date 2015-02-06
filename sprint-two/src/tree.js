b9e78bd5dfb39ff67ecfee442067f377829bbdf7
rjatkinson2 <rjatkinson2@gmail.com>
2015-02-06 12:49:27 -0800

    Completed tree function definition.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,27 +2,29 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target){
-
+treeMethods.contains = function(target, node){
+  node = node || this;
+  var test = (node.value === target);
+  if(node.children.length !== 0){
+    for (var i = 0; i < node.children.length; i++) {
+      test = test || this.contains(target, node.children[i]);
+    }
+  }
+  return test;
 };
-
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
