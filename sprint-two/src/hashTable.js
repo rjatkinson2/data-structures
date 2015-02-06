87931ee6a1472b7a089b96e3e0a2ef4cb712f143
rjatkinson2 <rjatkinson2@gmail.com>
2015-02-06 12:12:18 -0800

    implement hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,17 +5,32 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage[i] === undefined || this._storage[i] === [null,null]){
+    this._storage[i] = [[k,v]];
+  }else{
+    this._storage[i].push([k,v]);
+  }
 };
+
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  for(var j = 0; j < this._storage[i].length; j++){
+    if(this._storage[i][j][0] === k){
+      return this._storage[i][j][1];
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  for(var j = 0; j < this._storage[i].length; j++){
+    if(this._storage[i][j][0] === k){
+      this._storage[i][j] = [null,null];
+    }
+  }
 };
-
 
 
 /*
