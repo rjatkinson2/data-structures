0db52b03cbde90bc4f9309e77d1d4d8f6036e8f5
rjatkinson2 <rjatkinson2@gmail.com>
2015-02-05 16:27:55 -0800

    Completed linkedList.js function definition.  Need to rework to see if all cases are required.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    if (list.head === null) {
+      list.head = node;
+      list[value] = null;
+    } else if (list.tail === null) {
+      list.head.next = node;
+      list[value] = node;
+    } else {
+      list.tail.next = node;
+      list[value] = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function(){
+    var temp = list.head.value;
+    delete list[list.head.value];
+    list.head = list.head.next;
+
+    return temp;
   };
 
   list.contains = function(target){
+    var lookup = JSON.stringify(target);
+    return (list.hasOwnProperty(lookup));
   };
 
   return list;
